db5252d3b70d159ce00a1c3a3e89ff430c840016
NatalyaAlexeyev <natalya.alexeyev@gmx.de>
2024-11-28 21:14:58 +0100

    new dateis

---
+++ src/test/java/doctor/tests/CucumberRunnerTest.java
@@ -0,0 +1,10 @@
+package doctor.tests;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = {"src/test/resources/features"}, glue = {"hooks", "steps"})
+public class CucumberRunnerTest {
+}
