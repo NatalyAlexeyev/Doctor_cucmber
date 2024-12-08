092593f629174dbe35e425173d162ed8011c7ff8
NatalyaAlexeyev <natalya.alexeyev@gmx.de>
2024-12-08 22:12:12 +0100

    new

--- src/test/java/doctor/TestRunner.java
+++ src/test/java/doctor/TestRunner.java
@@ -10,10 +10,10 @@ import org.junit.runner.RunWith;
                 features = "src/test/resources/features",
                 glue = "doctor.stepsDefinitions",
                 plugin = {"pretty", "json:target/cucumber.json"},
-                tags= "@Login"
-//                tags = "@UberMich or @Contact or @Team or @Logout or @ProfileVorNaname or @ProfileNachNaname or " +
-//                        "@ProfileTelefonnummer or @Terminedelet or @Login or @InvalidPassword or @Registration or " +
-//                        "@UsercliksAnmelden or @Infusionstherapienewuser or @Infusionstherapie"
+                //tags= "@Login"
+                tags = "@UberMich or @Contact or @Team or @Logout or @ProfileVorNaname or @ProfileNachNaname or " +
+                        "@ProfileTelefonnummer or @Terminedelet or @Login or @InvalidPassword or @Registration or " +
+                        "@UsercliksAnmelden or @Infusionstherapienewuser or @Infusionstherapie"
         )
 
 public class TestRunner {
